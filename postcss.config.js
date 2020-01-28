5839329083c67a89029669799b9cf4dcebf93f30
Adrian Rosario <adrian@factorial.io>
2020-01-28 21:48:58 +0100

    chore: setup purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,19 @@
+const postcssPurgecss = require("@fullhuman/postcss-purgecss");
+
+const purgecss = postcssPurgecss({
+  content: ["./public/**/*.html", "./src/**/*.vue"],
+  defaultExtractor: content => content.match(/[\w-/:](?<!:)/g) || [],
+  whitelistPatterns: [
+    /-(leave|enter|appear)(|-(to|from|active))$/,
+    /^(?!(|.*?:)cursor-move).-move$/,
+    /^router-link(|-exact)-active$/
+  ]
+});
+
 module.exports = {
   plugins: [
-    // ...
     require("tailwindcss"),
-    require("autoprefixer")
-    // ...
+    require("autoprefixer"),
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
   ]
 };
