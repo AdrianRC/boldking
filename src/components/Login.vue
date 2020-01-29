73cfa8a2fefd13a7e06662938600caa52c2ed0c4
Adrian Rosario <adrian@factorial.io>
2020-01-29 08:19:47 +0100

    feat: extend login form with client and server validation

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,21 +1,103 @@
 <template>
-  <form action="">
+  <form class="mt-6" @submit="submit">
     <div>
       <label for="username">Username</label>
-      <input type="text" name="username" id="username" required />
+      <input
+        type="text"
+        name="username"
+        id="username"
+        placeholder="Please input"
+        pattern="^[A-Za-z0-9]+(?:[_-][A-Za-z0-9]+)*$"
+        minlength="8"
+        title="May only contain alphanumeric characters and underscores or hyphens as internal separators"
+        required
+        v-model="username"
+      />
     </div>
     <div>
       <label for="password">Password</label>
-      <input type="password" name="password" id="password" required />
+      <input
+        type="password"
+        name="password"
+        id="password"
+        placeholder="Please input"
+        minlength="8"
+        title="Must contain at least one number"
+        pattern=".*[0-9].*"
+        required
+        v-model="password"
+      />
     </div>
-    <input type="submit" value="Login" />
+    <div class="w-24 h-24" :class="statusClass">{{ message }}</div>
+    <input
+      type="submit"
+      value="Login"
+      :disabled="!isSubmitEnabled"
+      class="disabled:opacity-25 bg-green-400"
+    />
   </form>
 </template>
 
 <script lang="ts">
-import { createComponent } from "@vue/composition-api";
+import { createComponent, ref, computed } from "@vue/composition-api";
+import { validate, submit as post } from "../../api";
+
+enum Status {
+  Fail,
+  Neutral,
+  Success
+}
+
 export default createComponent({
   name: "Login",
-  setup() {}
+  setup() {
+    const username = ref("");
+    const password = ref("");
+    const message = ref("");
+    const status = ref(Status.Neutral);
+
+    const isSubmitEnabled = computed(() => {
+      const minLength = 8;
+      return (
+        username.value.length >= minLength && password.value.length >= minLength
+      );
+    });
+
+    async function submit(evt: Event) {
+      evt.preventDefault();
+      try {
+        await validate(username.value);
+        const res = await post({
+          username: username.value,
+          password: password.value
+        });
+        message.value = "Success!";
+        status.value = Status.Success;
+      } catch (err) {
+        message.value = err;
+        status.value = Status.Fail;
+      }
+    }
+
+    const statusClass = computed(() => {
+      switch (status.value) {
+        case Status.Success:
+          return "bg-green-500";
+        case Status.Fail:
+          return "bg-red-500";
+        default:
+          return "bg-white";
+      }
+    });
+
+    return {
+      username,
+      password,
+      submit,
+      message,
+      statusClass,
+      isSubmitEnabled
+    };
+  }
 });
 </script>
